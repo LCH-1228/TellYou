dc46e78159205c98c65497b48a097abeb7497de4
Chanho Lee <chanho1228@gmail.com>
2025-03-04 21:53:24 +0900

    두번째 페이지 추가 및 첫번째 두번째 페이지 연결

---
+++ TellYou/SecondViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.swift
+//  TellYou
+//
+//  Created by Mycom on 3/4/25.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    @IBAction func character1Button(_ sender: Any) {
+        print("character1Button clicked")
+    }
+    @IBAction func character2Button(_ sender: Any) {
+        print("character2Button clicked")
+    }
+    @IBAction func character3Button(_ sender: Any) {
+        print("character3Button clicked")
+    }
+    @IBAction func character4Button(_ sender: Any) {
+        print("character4Button clicked")
+    }
+    
+}

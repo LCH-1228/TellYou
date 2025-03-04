dc53543074a365430db8eafeb209e01f9e750cfa
Mitti Attar <chanho1228@gmail.com>
2025-03-04 11:49:21 +0900

    Initial Commit

---
+++ TellYou/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TellYou
+//
+//  Created by Mycom on 3/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

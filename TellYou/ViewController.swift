80b695363a30a0278d72cd9487594b6f2ac1ea71
Chanho Lee <chanho1228@gmail.com>
2025-03-05 13:27:37 +0900

    두번째 페이지 구현

--- TellYou/ViewController.swift
+++ TellYou/ViewController.swift
@@ -13,7 +13,21 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func character1Button(_ sender: Any) {
+        print("character1Button is clicked")
+    }
+    
+    @IBAction func character2Button(_ sender: Any) {
+        print("character2Button is clicked")
+    }
+    
+    @IBAction func character3Button(_ sender: Any) {
+        print("character3Button is clicked")
+    }
+    
+    @IBAction func character4Button(_ sender: Any) {
+        print("character4Button is clicked")
+    }
 }
 

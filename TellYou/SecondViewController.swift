d8cfac258e319c276dbdb75136dea14dc4f5b205
kimjaewoo522 <kimjaewoo522@gmail.com>
2025-03-05 20:03:59 +0900

    ScrollView test.md

---
+++ TellYou/SecondViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SecondViewController.swift
+//  TellYou
+//
+//  Created by 김재우 on 3/5/25.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    
+    }
+    
+}

5a5d1fa562360e1e4d18659b07e68795c3bc8de2
ghnn-n <cgh6470@gmail.com>
2025-03-05 10:46:58 +0900

    first commit

--- TellYou/ViewController.swift
+++ TellYou/ViewController.swift
@@ -9,11 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var test = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+test = "1234"
 
 }
 
